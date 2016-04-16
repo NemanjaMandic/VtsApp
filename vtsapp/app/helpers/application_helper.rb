633fbf4c448ad25bb32641b8a8b28457099e24ef
NemanjaMandic <nemanjans84@gmail.com>
2016-04-16 02:55:23 +0200

    products

---
+++ vtsapp/app/helpers/application_helper.rb
@@ -0,0 +1,27 @@
+module ApplicationHelper
+
+	def izdrkaj
+	puts "Ja cu prebrojati svoje pilice"
+	puts "Hens #{25 + 30 / 6}"
+	puts "Rusteri #{100 - 25 * 3 % 4}"
+
+	puts "Ja cu prebrojati jaja"
+
+	puts 3+2+1-5+4%2-1/4+6
+
+	puts "Istina je da je 3+2 < 5-7?"
+
+	puts 3+2 < 5-7
+
+	puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+end
+end
