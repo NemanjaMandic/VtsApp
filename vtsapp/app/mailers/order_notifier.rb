e0c55336ee92b97210dde66c9eb6b952000cecb4
NemanjaMandic <nemanjans84@gmail.com>
2016-05-06 16:18:06 +0200

    mailer

---
+++ vtsapp/app/mailers/order_notifier.rb
@@ -0,0 +1,26 @@
+class OrderNotifier < ApplicationMailer
+
+ default from: 'Sam Ruby <vtsapp@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'VtsApp Store Order'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+   @order = order 
+
+
+    mail to: order.email, subject: 'Vtsapp Store Order Shipped'
+  end
+end
